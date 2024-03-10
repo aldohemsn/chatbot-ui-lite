714151ad2de25e482059d4a27f5c1e3411892712
aldo <aldohe@msn.com>
2024-03-10 18:11:19 +0800

    umami inserted

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -4,11 +4,12 @@ export const Navbar: FC = () => {
   return (
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
       <div className="font-bold text-3xl flex items-center">
+	<script async src="https://umami.laterm.xyz/script.js" data-website-id="7ada8d5d-0ba3-447e-ac34-3b3e05ba1f29"></script>
         <a
           className="ml-2 hover:opacity-50"
-          href="https://code-scaffold.vercel.app"
+          title=""
         >
-          Chatbot UI
+          🌸🌼🌻🌺🌹🌷
         </a>
       </div>
     </div>
