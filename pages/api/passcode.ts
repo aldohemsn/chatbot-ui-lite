6865bb9ddf6cbd7e608294733ce0c390612e4c0d
aldo <aldohe@msn.com>
2024-09-07 17:31:21 +0800

    xx

--- pages/api/passcode.ts
+++ pages/api/passcode.ts
@@ -1,4 +1,6 @@
 // pages/api/passcode.ts
-export default function handler(req, res) {
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ passcode: process.env.NEXT_PUBLIC_PASSCODE });
 }
